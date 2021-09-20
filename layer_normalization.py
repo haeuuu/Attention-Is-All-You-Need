0269459b2c5a9609aa2b5b2af167eb61dbdce323
haeuuu <srhuu15@naver.com>
2021-09-21 01:47:41 +0900

    create : layer_normalization.py

---
+++ layer_normalization.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+class LayerNormalization(nn.Module):
+    def __init__(self, dim):
+        """Layer Normalization
+        paper : https://arxiv.org/pdf/1607.06450.pdf
+
+        Parameters
+        ----------
+        dim : tuple
+        gamma : torch.FloatTensor
+            scale factor
+        beta : torch.FloatTensor
+            shift factor
+        """
+        super().__init__()
+        self.gamma = nn.Parameter(torch.Tensor(*dim))
+        self.beta = nn.parameter(torch.Tensor(*dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.gamma.data.fill_(1.)
+        self.beta.data.fill_(0.)
+
+    def forward(self, x):
+        mean = x.mean(dim = -1)
+        std = ((x- mean) ** 2).mean(dim = -1).sqrt()
+        normalized = (x - mean) / std
+
+        return self.gamma * normalized + self.beta
