16076d1773f306b9ceea8fd3c1229fcfffac77d8
haeuuu <srhuu15@naver.com>
2021-09-24 02:56:37 +0900

    refactor : change parameter type to tensor

--- utils.py
+++ utils.py
@@ -1,17 +1,30 @@
 import torch
 
-def generate_lm_mask(seq_len, fill = 1.):
+def generate_lm_mask(inputs, fill = 1.):
     """Generate mask for language modeling
+
+    Returns
+    -------
+    mask : (batch_size, seq_len, seq_len)
+
     Examples
     ---------
-    generate_mask(max_len = 4, fill = 1.)
-    tensor([[0, 1, 1, 1, 1],
-            [0, 0, 1, 1, 1],
-            [0, 0, 0, 1, 1],
-            [0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0]])
+    inputs = torch.LongTensor([[5,2,3],
+                                [9,8,3]]) ; token indices
+
+    generate_lm_mask(inputs, fill = 1)
+    tensor([[[0, 1, 1],
+            [0, 0, 1],
+            [0, 0, 0]],
+
+            [[0, 1, 1],
+            [0, 0, 1],
+            [0, 0, 0]]])
     """
+    batch_size, seq_len = inputs.shape[0], inputs.shape[1]
+
     mask = torch.full((seq_len, seq_len), fill).triu()
     mask = mask.fill_diagonal_(0.)
+    mask = mask.repeat(batch_size, 1, 1)
 
-    return mask
+    return mask.bool().to(inputs.device)
